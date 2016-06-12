9d1af105fbac44c77a54fd34c6088e80bbd731c6
mazharulcste <mazharulislamcste@gmail.com>
2016-06-12 09:53:44 +0600

    res group web site

---
+++ res group/ceo message/index.php
@@ -0,0 +1,44 @@
+<?php include ("../header/header1.php"); ?>    
+    
+    <!-- container -->
+    <div id="main-fullwidth">      
+    <div class="container" style="max-width:960px;min-width:960px;margin-left:-15px;">
+    
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 wow fadeInDown">
+                    <div class="testimonial">
+                        <h2 style="font-size:26px;"> CEO MESSAGE </h2>
+                        <div class="media testimonial-inner" style="margin-top:30px;">
+                            <div class="pull-left">
+                                <img class="img-responsive img-circle" style="height:250px; weight:300px;" src="../admin/asset/home_gallery/DJPJR5835A.jpg">
+                            </div>
+                            <div class="media-body">
+                                <p align="justify">
+                                    <div class="media-body">
+<p style="text-align:justify"><strong>W</strong>elcome to our website. I hope you find it informative, interesting and clear. We have been in business since 1992, which provides us the opportunity to review and reflect on the cycles we have encountered and what we have learned about our industry and ourselves. In the past, we focused first on trading, service oriented business, etc., and then we learned our greatest success came by letting our people pursue their dreams for the company while management provided the support for those dreams. What we found were results far greater than we had imagined. In order to carry out this philosophy, we have a company with 9 divisions that are supported, rather than managed, by our Private Limited Company called RES GROUP. The magic of this approach has been that all employees can get their arms around the group they are in, and they can see the difference they make. It also puts our clients in direct contact with all of the decision-makers that affect their project. We are proud of our people and where they are taking our company. With our decentralized form of management, it is imperative that we have people who both reflect the culture of the company and are dedicated, intelligent leaders. We strongly support an approach of promoting from within and encouraging our people to follow their interests. For our employees, we dedicate ourselves to providing an environment that welcomes their creativity and new ideas, provides challenges for them to strive to be their best, and allows our people to find and follow their passions. Our employees are the future of our company.</p>
+
+<p style="text-align:justify"><strong>-Md. Reza-E-Selim /</strong> CEO of RES Group</p>
+</div>
+  
+                                </p>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+
+            </div><!--/.row-->
+        
+    
+    </div>
+
+
+    <!--end container -->
+       
+                    
+    </div><!-- #main-fullwidth -->
+    
+    
+<?php include("../footer/footer1.php"); ?>
